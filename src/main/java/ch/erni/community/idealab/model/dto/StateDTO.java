a53764ce00364709eaa15aaf1f860252de0f6523
Pavol Rajzak <pavol.rajzak@erni.sk>
2015-02-06 10:17:00 +0100

    Added .gitignore

--- src/main/java/ch/erni/community/idealab/model/dto/StateDTO.java
+++ src/main/java/ch/erni/community/idealab/model/dto/StateDTO.java
@@ -4,4 +4,7 @@ package ch.erni.community.idealab.model.dto;
  * @author rap
  */
 public class StateDTO {
+
+	// TODO @rap: later
+
 }
--- src/main/java/ch/erni/community/idealab/model/dto/UserDTO.java
+++ src/main/java/ch/erni/community/idealab/model/dto/UserDTO.java
@@ -4,4 +4,7 @@ package ch.erni.community.idealab.model.dto;
  * @author rap
  */
 public class UserDTO {
+
+	// TODO @rap: later
+
 }
